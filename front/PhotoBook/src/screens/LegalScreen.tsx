0d178815828a32ae016c0375a8f8d77b552ab8f6
Axel FAUGAS <axel.faugas@yahoo.fr>
2022-06-10 10:02:21 +0200

    dev: trucs

--- front/PhotoBook/src/screens/LegalScreen.tsx
+++ front/PhotoBook/src/screens/LegalScreen.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 
 const LegalScreen = () => {
-    return (
-        <View style={styles.mainContainer}>
-            <Text style={styles.text}>Legal</Text>
-        </View>
-    );
+  return (
+    <View style={styles.mainContainer}>
+      <Text style={styles.text}>Legal</Text>
+      <Image style={styles.image} source={require('../../assets/legal.png')} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    text: {
-      fontWeight: 'bold',
-      fontSize: 50,
-    },
-    mainContainer: {
-      alignItems: 'center',
-      height: '100%',
-      justifyContent: 'center',
-    },
-  });
+  text: {
+    fontWeight: 'bold',
+    fontSize: 50,
+  },
+  mainContainer: {
+    alignItems: 'center',
+    height: '100%',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50
+  }
+});
 
 export default LegalScreen;
